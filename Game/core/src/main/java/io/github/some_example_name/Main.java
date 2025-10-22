9c69fdf2288a3d43827c38a05daab0e12af811ac
Zoey Ahmed <zoethetransrat@gmail.com>
2025-10-22 09:30:33 +0100

    game: Add rendering for the tilemap to the game.

--- Game/core/src/main/java/io/github/some_example_name/Main.java
+++ Game/core/src/main/java/io/github/some_example_name/Main.java
@@ -2,33 +2,47 @@ package io.github.some_example_name;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    // Map rendering.
+    OrthographicCamera camera;
+    TiledMap tiledMap;
+    TiledMapRenderer mapRenderer;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        /* 
+         * Instantiate all the objects needed for rendering the TileMap and the player sprite.
+         * No parameters or returns. 
+         */
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+
+        tiledMap = new TmxMapLoader().load("Game Map.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
     }
 
     @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
-    }
+    public void dispose() {}
 }
