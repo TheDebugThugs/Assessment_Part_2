e329d7653ce6113e092a1c4dbb5973a2db142579
Jenny Bryant <jennygbryant@iCloud.com>
2025-12-05 04:36:54 +0000

    setup: created test folder

---
+++ Game/core/src/test/java/io/github/some_example_name/TestTest.java
@@ -0,0 +1,11 @@
+package io.github.some_example_name;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+  @Test
+  void testWorking() {
+    assertEquals(2, 1 + 1);
+  }
+}
